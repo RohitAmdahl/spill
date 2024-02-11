721792455bd86619c0189999c29ae0b9347b4deb
rohitAmdahl <rohit_designer@outlook.com>
2024-02-12 00:31:48 +0100

    only picture added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log("hello");
+const picture = () => {
+  const canvas = document.querySelector(".myCanvas");
+  const ctx = canvas.getContext("2d");
+  const img = new Image();
+  img.src = "../img/red.png";
+  img.onload = () => {
+    ctx.drawImage(img, 130, 100, 40, 40);
+  };
+};
+const image = picture();
+console.log(image);
+console.log(picture());
+
+const draw = () => {
+  const canvas = document.querySelector(".myCanvas");
+  const shape = canvas.getContext("2d");
+};
+
+console.log(draw());
