85d46e49bfb67461682bc6b6009d4b534483454a
rohitAmdahl <rohit_designer@outlook.com>
2024-02-12 15:55:00 +0100

    editing the code and made globel variable  the code

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
+const canvas = document.querySelector(".myCanvas");
+const context = canvas.getContext("2d");
+
 const picture = () => {
-  const canvas = document.querySelector(".myCanvas");
-  const ctx = canvas.getContext("2d");
   const img = new Image();
   img.src = "../img/red.png";
   img.onload = () => {
-    ctx.drawImage(img, 130, 100, 40, 40);
+    context.drawImage(img, 130, 100, 40, 40);
   };
 };
 const image = picture();
 console.log(image);
 console.log(picture());
 
-const draw = () => {
-  const canvas = document.querySelector(".myCanvas");
-  const shape = canvas.getContext("2d");
-};
+const draw = () => {};
 
 console.log(draw());
