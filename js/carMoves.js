fcd4317b02be1b5b26f57174160b8e520ff17386
rohitAmdahl <rohit_designer@outlook.com>
2024-02-13 00:09:14 +0100

    made a file

---
+++ js/carMoves.js
@@ -0,0 +1,8 @@
+// import { context } from "../js/main.js";
+// export const backgroundImg = () => {
+//   const backgroundPic = new Image();
+//   backgroundPic.src = "../img/road.png";
+//   backgroundPic.onload = () => {
+//     context.drawImage(backgroundPic, 40, 40, 100, 100);
+//   };
+// };
