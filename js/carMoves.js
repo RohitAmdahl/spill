d8592ed647bfef04643ece57cd7a70fe6dd8eb70
Rohit kumar Amdahl <91061651+RohitAmdahl@users.noreply.github.com>
2024-02-15 14:21:42 +0100

    added comment

--- js/carMoves.js
+++ js/carMoves.js
@@ -1,8 +1,1 @@
-// import { context } from "../js/main.js";
-// export const backgroundImg = () => {
-//   const backgroundPic = new Image();
-//   backgroundPic.src = "../img/road.png";
-//   backgroundPic.onload = () => {
-//     context.drawImage(backgroundPic, 40, 40, 100, 100);
-//   };
-// };
+// code will be added when i started working on it 
