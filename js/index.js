a3c35e12f18eec63dbfa1e24de6f3a0d56edc62d
rohitAmdahl <rohit_designer@outlook.com>
2024-02-23 00:06:25 +0100

    remove start game

--- js/index.js
+++ js/index.js
@@ -1,13 +1,6 @@
 const restart = document.querySelector(".Restart-btn");
 console.log(restart);
-const start = document.querySelector(".start-btn");
 
-export const startGame = () => {
-  start.addEventListener("click", () => {
-    start.innerHTML = "stop";
-  });
-  return start;
-};
 export const restartGame = () => {
   restart.addEventListener("click", () => {
     setInterval(() => {
