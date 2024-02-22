3b3d5450f47be81fe5ed3e5fec93742801567964
rohitAmdahl <rohit_designer@outlook.com>
2024-02-22 23:21:27 +0100

    car moves left and right

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ export const canvas = document.querySelector(".myCanvas");
 export const context = canvas.getContext("2d");
 
 const car = yellow();
+console.log(car);
 const game = startGame();
 const restart = restartGame();
 
@@ -15,21 +16,66 @@ let spriteObject = {
   height: 35,
 };
 
+const img = new Image();
+img.src = "../img/red.png";
 let speedX = 0;
 let speedY = 0;
 let moveLeft = false;
 let moveRight = false;
-const img = new Image();
-img.src = "../img/red.png";
-const redCar = () => {
-  img.onload = () => {
-    context.drawImage(
-      img,
-      spriteObject.x,
-      spriteObject.y,
-      spriteObject.width,
-      spriteObject.height
-    );
-  };
+img.onload = () => {
+  context.drawImage(
+    img,
+    spriteObject.x,
+    spriteObject.y,
+    spriteObject.width,
+    spriteObject.height
+  );
 };
-const image = redCar();
+window.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft") {
+    moveLeft = true;
+  }
+  if (e.key === "ArrowRight") {
+    moveRight = true;
+  }
+});
+window.addEventListener("keyup", (e) => {
+  if (e.key === "ArrowLeft") {
+    moveLeft = false;
+  }
+  if (e.key === "ArrowRight") {
+    moveRight = false;
+  }
+});
+
+function update() {
+  if (moveLeft) {
+    speedX = -1;
+  }
+  if (moveRight) {
+    speedX = 1;
+  }
+  if (!moveLeft && !moveRight) {
+    speedX = 0;
+  }
+  if (spriteObject.x < 0) {
+    spriteObject.x = 0;
+  }
+
+  spriteObject.x += speedX;
+  spriteObject.y += speedY;
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(
+    img,
+    spriteObject.x,
+    spriteObject.y,
+    spriteObject.width,
+    spriteObject.height
+  );
+  car;
+  game;
+  restart;
+  requestAnimationFrame(update);
+}
+
+update();
